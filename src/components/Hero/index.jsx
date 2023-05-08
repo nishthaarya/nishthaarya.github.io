63b2da653def2b6dfc67f1b3297131bdbd1372ec
Nishtha Arya <aryanishthaa@gmail.com>
2023-05-08 19:57:47 +0530

    small fix in mobile screen

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -6,7 +6,7 @@ import { PopupButton } from "@typeform/embed-react";
 
 function Hero() {
     return (
-        <div className='mx-auto w-[80vw] flex justify-center p-10 bg-hotpink my-20 sm:my-8 sm:p-4 sm:h-[60vh] md:my-8 md:h-[60vh]  rounded-xl h-[80vh]'>
+        <div className='mx-auto w-[80vw] flex justify-center p-10 bg-hotpink my-20 sm:my-8 sm:p-4 sm:h-[70vh] md:my-8 md:h-[60vh]  rounded-xl h-[80vh]'>
             <div className='border-2 border-beige rounded-xl w-full flex flex-row justify-center p-10 sm:p-4'>
                 <div className='flex my-auto flex-col space-y-10 mr-10 sm:mr-0 sm:space-y-2 md:space-y-2 md:mr-0'>
                     <div className='text-8xl w-1/3 sm:text-6xl sm:w-[100%] text-beige font-extrabold sm:text-center px-4 my-auto'>Are <br /> you <br /> ready <br /> to <br /> <span className=' font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-beige to-salmon'>unbox?</span>
